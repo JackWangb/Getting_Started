86fa9a03f4dd88b12ea22527324e96c85f21cef3
wang_bin <wang_bin@gionee.com>
2015-10-20 19:58:08 +0800

    Activity的生命周期展示

--- app/src/main/java/training/course/getting_started/LifecycleActivity.java
+++ app/src/main/java/training/course/getting_started/LifecycleActivity.java
@@ -1,6 +1,8 @@
 package training.course.getting_started;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,7 +18,6 @@ public class LifecycleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lifecycle);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         Logs.d(TAG+" onCreate");
-//        toolbar.set
 
         setSupportActionBar(toolbar);
 
@@ -28,8 +29,83 @@ public class LifecycleActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        setupViews();
     }
 
+    private void setupViews() {
+        findViewById(R.id.lifecycle_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LifecycleActivity.this,CustomActionBarActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Logs.d(TAG+" onStart");
+    }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Logs.d(TAG+" onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Logs.d(TAG+" onResume");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Logs.d(TAG+" onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Logs.d(TAG+" onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Logs.d(TAG+" onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Logs.d(TAG+" onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Logs.d(TAG+" onDestory");
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        Logs.d(TAG+" onTrimMemory level:"+level);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        Logs.d(TAG + " onLowMemory");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Logs.d(TAG+" onActivityResult");
+    }
 }
